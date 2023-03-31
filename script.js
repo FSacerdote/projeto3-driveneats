8e90d0e0e7b23e4d1cd00c1b45ba5483770f9c20
Fernando_Sacerdote <fernando.saerdote@gmail.com>
2023-03-30 23:56:06 -0300

    Botao ligando quando seleciona todos os itens

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ function seleciona(elemento, pai){
     if(selecionadoAnteriormente !== null){
         selecionadoAnteriormente.classList.remove("selecionado")
         let icone = selecionadoAnteriormente.querySelector("ion-icon");
-        icone.classList.remove("aparece");
+        icone.classList.remove("aparece");        
     }
     elemento.classList.add("selecionado")
     icone = elemento.querySelector("ion-icon");
     icone.classList.add("aparece")
+    if(document.querySelector(".prato .selecionado") !== null && document.querySelector(".sobremesa .selecionado") !== null && document.querySelector(".bebida .selecionado") !== null){
+        ligaBotao();
+    }
+}
+
+function ligaBotao(){
+    let botao = document.querySelector("button")
+    botao.innerHTML = "Fechar Pedido";
+    botao.classList.add("botaoLigado");
+    botao.disabled = false;
 }
