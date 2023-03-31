453ab3b52743422a6ead6540a70407490ce5b295
Fernando_Sacerdote <fernando.saerdote@gmail.com>
2023-03-31 02:08:47 -0300

    Botão funcionando - mandando o resumo da compra para o whats

--- script.js
+++ script.js
@@ -15,7 +15,21 @@ function seleciona(elemento, pai){
 
 function ligaBotao(){
     let botao = document.querySelector("button")
+    let link = document.querySelector("a");
     botao.innerHTML = "Fechar Pedido";
     botao.classList.add("botaoLigado");
     botao.disabled = false;
+    link.disabled = false;
+}
+
+function fechaPedido(){
+    const prato = document.querySelector(".prato .selecionado");
+    const bebida = document.querySelector(".bebida .selecionado");
+    const sobremesa = document.querySelector(".sobremesa .selecionado");
+    const precoPrato = (prato.querySelector(".preco").innerHTML).replace("R$ ", "").replace(",", ".");
+    const precoBebida = (bebida.querySelector(".preco").innerHTML).replace("R$ ", "").replace(",", ".");
+    const precoSobremesa = (sobremesa.querySelector(".preco").innerHTML).replace("R$ ", "").replace(",", ".");
+    const total = (Number(precoBebida) + Number(precoPrato) + Number(precoSobremesa)).toFixed(2);
+    const mensagem = `Olá, gostaria de fazer o pedido:\n- Prato: ${prato.querySelector("p").innerHTML}\n- Bebida: ${bebida.querySelector("p").innerHTML}\n- Sobremesa: ${sobremesa.querySelector("p").innerHTML}\nTotal: R$ ${total.replace(".",",")}`;  
+    window.open("https://wa.me/5519992926745?text=" + encodeURIComponent(mensagem));
 }
